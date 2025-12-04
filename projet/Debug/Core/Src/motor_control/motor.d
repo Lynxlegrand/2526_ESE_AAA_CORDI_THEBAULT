9e443cf77679bdb120c2117c8d63e1b3dec22eb8
Hugo cordi <hugo.cordi@ensea.fr>
2025-12-04 08:11:32 +0100

    nouveau gitignore

--- projet/Debug/Core/Src/motor_control/motor.d
+++
@@ -1,3 +0,0 @@
-Core/Src/motor_control/motor.o: ../Core/Src/motor_control/motor.c \
- D:/Githubs/2526_ESE_AAA_CORDI_THEBAULT/projet/Core/Inc/motor_control/motor.h
-D:/Githubs/2526_ESE_AAA_CORDI_THEBAULT/projet/Core/Inc/motor_control/motor.h:
